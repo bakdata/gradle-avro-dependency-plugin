141fae7721b05138fe79f249a9ab697e4dfac882
Philipp Schirmer <philipp.schirmer@bakdata.com>
2022-02-22 15:23:46 +0100

    Create Avro Dependency Plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,10 +44,9 @@ subprojects {
     apply(plugin = "java")
 
     dependencies {
-        "testRuntimeOnly"("org.junit.jupiter:junit-jupiter-engine:5.3.0")
-        "testImplementation"("org.junit.jupiter:junit-jupiter-api:5.3.0")
-        "testImplementation"("org.assertj", "assertj-core", "3.11.1")
-        "testImplementation"("org.junit-pioneer", "junit-pioneer", "0.3.0")
+        "testRuntimeOnly"("org.junit.jupiter:junit-jupiter-engine:5.7.2")
+        "testImplementation"("org.junit.jupiter:junit-jupiter-api:5.7.2")
+        "testImplementation"("org.assertj", "assertj-core", "3.20.2")
     }
 }
 
