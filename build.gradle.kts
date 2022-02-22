563ea2db8eb08495aa5801fe3c56ebe12b1ca181
Philipp Schirmer <philipp.schirmer@bakdata.com>
2022-02-22 14:33:34 +0100

    Create Avro Dependency Plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,6 @@ buildscript {
 plugins {
     // release
     id("net.researchgate.release") version "2.8.1"
-    // eat your own dog food - apply the plugins to this plugin project
     id("com.bakdata.sonar") version "1.1.7"
     id("com.bakdata.sonatype") version "1.1.7"
     id("org.hildan.github.changelog") version "0.8.0"
