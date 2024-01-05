25a4189f40f4134556e7dafea64cdb5ca8764480
Philipp Schirmer <philipp.schirmer@bakdata.com>
2024-01-05 08:51:14 +0100

    Upgrade Avro plugin to 1.9.1

--- avro/build.gradle.kts
+++ avro/build.gradle.kts
@@ -7,5 +7,5 @@ apply(plugin = "org.gradle.kotlin.kotlin-dsl")
 description = "A Gradle plugin that lets you compile Apache Avro schemas to Java classes and supports dependencies"
 
 dependencies {
-    implementation(group = "com.github.davidmc24.gradle.plugin", name = "gradle-avro-plugin", version = "1.5.0")
+    implementation(group = "com.github.davidmc24.gradle.plugin", name = "gradle-avro-plugin", version = "1.9.1")
 }
