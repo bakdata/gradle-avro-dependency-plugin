d61ed9203bd26b89390f0bc3060170f69503c08c
Philipp Schirmer <philipp.schirmer@bakdata.com>
2024-02-22 13:07:41 +0100

    Upgrade to Gradle 8.4

--- avro/build.gradle.kts
+++ avro/build.gradle.kts
@@ -1,11 +1,12 @@
 plugins {
-    kotlin("jvm")
+    kotlin("jvm") version "1.5.21"
     id("org.jetbrains.dokka") version "1.9.10"
 }
-apply(plugin = "org.gradle.kotlin.kotlin-dsl")
 
 description = "A Gradle plugin that lets you compile Apache Avro schemas to Java classes and supports dependencies"
 
 dependencies {
+    implementation(gradleApi())
+    implementation(gradleKotlinDsl())
     implementation(group = "com.github.davidmc24.gradle.plugin", name = "gradle-avro-plugin", version = "1.9.1")
 }
