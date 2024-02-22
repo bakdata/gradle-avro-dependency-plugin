3b83974481fa58c61759160e853d5b325c07f7fb
Philipp Schirmer <philipp.schirmer@bakdata.com>
2024-02-22 12:21:35 +0100

    Upgrade to Gradle 8.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,19 @@
+import net.researchgate.release.GitAdapter.GitConfig
+import net.researchgate.release.ReleaseExtension
+
 buildscript {
     dependencies {
-        classpath("org.gradle.kotlin:gradle-kotlin-dsl-plugins:4.1.2")
-        classpath("com.gradle.publish:plugin-publish-plugin:1.2.1")
+        classpath("org.gradle.kotlin:gradle-kotlin-dsl-plugins:2.1.6")
+        classpath("com.gradle.publish:plugin-publish-plugin:0.11.0")
     }
 }
 
 plugins {
     // release
-    id("net.researchgate.release") version "3.0.2"
-    id("com.bakdata.sonar") version "1.1.17"
+    id("net.researchgate.release") version "2.8.1"
+    id("com.bakdata.sonar") version "1.1.14"
     id("com.bakdata.sonatype") version "1.1.14"
-    id("org.hildan.github.changelog") version "2.2.0"
+    id("org.hildan.github.changelog") version "0.8.0"
 }
 
 allprojects {
@@ -43,11 +46,6 @@ configure<org.hildan.github.changelog.plugin.GitHubChangelogExtension> {
 subprojects {
     apply(plugin = "java")
 
-    configure<JavaPluginExtension> {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
     dependencies {
         "testRuntimeOnly"("org.junit.jupiter:junit-jupiter-engine:5.7.2")
         "testImplementation"("org.junit.jupiter:junit-jupiter-api:5.7.2")
@@ -71,7 +69,6 @@ if (!version.toString().endsWith("-SNAPSHOT")) {
                             id = "com.bakdata.${project.name}"
                             implementationClass = "com.bakdata.gradle.${project.name.capitalize()}Plugin"
                             description = project.description
-                            displayName = "Bakdata $name plugin"
                         }
                     }
                 }
@@ -79,10 +76,23 @@ if (!version.toString().endsWith("-SNAPSHOT")) {
                 configure<com.gradle.publish.PluginBundleExtension> {
                     website = "https://github.com/bakdata/gradle-avro-dependency-plugin"
                     vcsUrl = "https://github.com/bakdata/gradle-avro-dependency-plugin"
-                    tags = listOf("bakdata", name)
+                    (plugins) {
+                        "${name.capitalize()}Plugin" {
+                            displayName = "Bakdata $name plugin"
+                            tags = listOf("bakdata", name)
+                        }
+                    }
                 }
             }
         }
+    }
+}
+
+fun ReleaseExtension.git(configure: GitConfig.() -> Unit) = (getProperty("git") as GitConfig).configure()
+
+release {
+    git {
+        requireBranch = "main"
     }
 }
 
