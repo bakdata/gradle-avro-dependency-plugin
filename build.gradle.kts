9e61380cdd54528b057e8f50605764bbce0f51a5
Philipp Schirmer <philipp.schirmer@bakdata.com>
2024-03-11 12:02:30 +0100

    Upgrade to Gradle 8.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,11 @@
-buildscript {
-    repositories {
-        maven {
-            url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots")
-        }
-    }
-    dependencies {
-        classpath("com.bakdata.gradle:sonatype:1.3.2-SNAPSHOT")
-    }
-}
-
 plugins {
     // release
     id("com.bakdata.release") version "1.3.0"
     id("com.bakdata.sonar") version "1.1.17"
+    id("com.bakdata.sonatype") version "1.3.2"
     id("org.gradle.kotlin.kotlin-dsl") version "4.1.2" apply false
     id("com.gradle.plugin-publish") version "1.2.1" apply false
 }
-apply(plugin = "com.bakdata.sonatype")
 
 allprojects {
     repositories {
